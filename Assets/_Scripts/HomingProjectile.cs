243381069728832663af8fc799d7f700e92bf1fd
Christian Dinh <Christian Dinh>
2016-02-07 14:40:12 -0600

    Implement missile homing

---
+++ Assets/_Scripts/HomingProjectile.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HomingProjectile : Projectile {
+
+    [SerializeField] private float agility;
+
+    private Transform tgt;
+	
+    void Start () {
+        Participant tgtObj = GameObject.FindGameObjectWithTag("Participants").GetComponent<ParticipantManager>().CurrentTarget;
+        if (tgtObj != null)
+            tgt = tgtObj.transform;
+    }
+
+	void FixedUpdate () {
+	    if (tgt != null) {
+            Quaternion tgtRotation = Quaternion.LookRotation (tgt.position - transform.position);
+            transform.rotation = Quaternion.RotateTowards (transform.rotation, tgtRotation, agility);
+        }
+        Move ();
+	}
+}
--- Assets/_Scripts/Projectile.cs
+++ Assets/_Scripts/Projectile.cs
@@ -3,15 +3,19 @@ using System.Collections;
 
 public class Projectile : MonoBehaviour {
 
-    [SerializeField] private float speed;
-    [SerializeField] private float ttl;
+    [SerializeField] protected float speed;
+    [SerializeField] protected float ttl;
 	
 	void FixedUpdate () {
-	    if (ttl > 0f) {
+        Move ();
+	}
+
+    protected void Move () {
+        if (ttl > 0f) {
             ttl -= Time.fixedDeltaTime;
             transform.position = transform.position + transform.forward * speed;
         } else {
-            Destroy (gameObject);
+            Destroy(gameObject);
         }
-	}
+    }
 }
